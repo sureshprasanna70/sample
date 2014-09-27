219349cd8e4cf64affdc50169b4ae4d7e7617da6
Prelang Builder <builder@prelang.com>
2014-09-27 05:30:25 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Service' to allow for create, update

--- app/admin/service.rb
+++ app/admin/service.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Service do
+  permit_params :home_start, :home_note, :home_date, :service_start, :service_start_note, :service_start_date, :service_complete, :service_complete_note, :leave_center, :leave_center_date, :deliver, :deliver_note, :deliver_date, :attachment
 
 
   # See permitted parameters documentation:
